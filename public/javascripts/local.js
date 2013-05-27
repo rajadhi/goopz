298dcdef23c926118fcd26a6a96f9404474d8871
Rajadhi Wickramatunga <rajadhi@live.com>
2013-05-27 23:31:44 +1000

    Jadifiying the index page

--- public/javascripts/local.js
+++ public/javascripts/local.js
@@ -1,7 +1,28 @@
-/**
- * Created with JetBrains WebStorm.
- * User: Rajadhi
- * Date: 27/05/13
- * Time: 9:03 PM
- * To change this template use File | Settings | File Templates.
- */
+function getMap() {
+    var map = new Microsoft.Maps.Map(document.getElementById('myMap'), {credentials: 'Ah0k8PKqcZTJsD12xoHSVy1kc-boQ53Vpj_zOlTiJup1yXMfVEmaXevdWEsCTqke'});
+    var locations = [];
+
+    $.ajax({
+        type: "GET",
+        url: "/../gpx/first.gpx",
+        dataType: "xml",
+        success: function (xml) {
+            var points = [];
+
+            $(xml).find("trkpt").each(function () {
+                var lat = $(this).attr("lat");
+                var lon = $(this).attr("lon");
+                var p = new Microsoft.Maps.Location(lat, lon);
+                locations.push(p);
+            });
+
+            var polyline = new Microsoft.Maps.Polyline(locations, null);
+
+            map.setView({ bounds: Microsoft.Maps.LocationRect.fromLocations(locations)});
+
+            map.setView({bounds: Microsoft.Maps.Loca});
+            map.entities.push(polyline);
+
+        }
+    });
+}
